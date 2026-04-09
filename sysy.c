081abd33dd9fc28cbae0a74dacd97a633a20feaa
RibosomeK <122797zhyktb@gmail.com>
2026-04-09 10:54:02 +0800

    parse main()

--- sysy.c
+++ sysy.c
@@ -5,16 +5,26 @@
 #include "ast.h"
 
 int main() {
-    char* program = "return 0;";
+    char* program = "int main() { return 0; }";
     Tokens tokens = {0};
     Lexer lexer = {.src = program, .pos = 0, .len = strlen(program)};
     LEX_parse(&lexer, &tokens);
-    TOKEN_display(&tokens);
+    Str buf = {0};
+    printf("=================TOKEN==================\n");
+    DA_foreach(&tokens, token) {
+        size_t idx = token - tokens.items;
+        TOKEN_to_str(token, &buf);
+        printf("%zu:%s\n", idx, buf.items);
+        STR_clear(&buf);
+    }
+    printf("=================AST====================\n");
     Parser parser = {.tokens = &tokens, .pos = 0};
-    Nodes nodes = {0};
-    AST_parse(&parser, &nodes);
-    DA_foreach(&nodes, node) {
-        NODE_display(node);
+    STR_clear(&buf);
+    while (PARSER_curr(&parser).type != TOK_EOF) {
+        Node node = AST_parse(&parser);
+        NODE_to_str(&node, &buf);
+        printf("%s\n", buf.items);
+        STR_clear(&buf);
     }
     return 0;
 }
